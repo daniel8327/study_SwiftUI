964f2ac7ea51222b30f65369a21a2e05c47e9c49
장태현 <danielchang27@gmail.com>
2021-04-15 09:41:11 +0900

    Initial Commit

---
+++ MyFirstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstSwiftUI
+//
+//  Created by moonkyoochoi on 2021/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
